b9b9caf11879c21b6687908a8584c6fe053f564c
smjeffery22 <parkjon8@gmail.com>
2022-03-26 22:55:32 -0400

    Add framer animaton on genre filter

--- src/components/list/PopularListItem.jsx
+++ src/components/list/PopularListItem.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import './PopularListItem.scss';
 
 const PopularListItem = ({ movie }) => {
 	return (
-		<div className="movie-item">
-			<div className="movie-item-card">
-				<img
-					className="movie-poster"
-					src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
-					alt=""
-				/>
-				<div className="movie-info">
-					<div className="movie-title">{movie.title}</div>
-          <div className="movie-rating">{movie.vote_average}</div>
+		<AnimatePresence>
+			<motion.div
+				className="movie-item"
+				whileHover={{ scale: 1.2 }}
+        animate={{ opacity: 1 }}
+				initial={{ opacity: 0 }}
+        exit={{ opacity: 0 }}
+				layout
+			>
+				<div className="movie-item-card">
+					<img
+						className="movie-poster"
+						src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+						alt=""
+					/>
+					<div className="movie-info">
+						<div className="movie-title">{movie.title}</div>
+						<div className="movie-rating">{movie.vote_average}</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			</motion.div>
+		</AnimatePresence>
 	);
 };
 
