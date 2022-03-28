e317b7a06b67e9c3072bf1ccca6cfefc9dbacf3a
smjeffery22 <parkjon8@gmail.com>
2022-03-28 12:57:13 -0400

    Updated navbar with react router and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from './components/navbar/Navbar';
 import PopularList from './components/list/PopularList';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="movie-wrap">
-      <Navbar />
-      <PopularList />
-    </div>
-  );
+	return (
+		<Router>
+			<div className="movie-wrap">
+				<Navbar />
+				<Routes>
+					<Route path="/popular" element={<PopularList />} />
+					<Route path="/now-playimg" element={<PopularList />} />
+					<Route path="/upcoming" element={<PopularList />} />
+					<Route path="/top-rated" element={<PopularList />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.scss';
 
 const Navbar = () => {
 	return (
 		<div className="navbar">
 			<div className="navbar-container">
-				<div className="navbar-left">MovieExplorer</div>
-				<div className="navbar-right">Jeffery Park</div>
+				<div className="navbar-left">
+					<Link to="/">MovieExplorer</Link>
+					<Link to="/popular">Popular</Link>
+					<Link to="/now-playing">Now Playing</Link>
+					<Link to="/upcoming">Upcoming</Link>
+					<Link to="/top-rated">Top Rated</Link>
+				</div>
+				<div className="navbar-right">Search</div>
 			</div>
 		</div>
 	);
